dd90ae5f8f7334979049e23fedecb9dee2482dc1
meng <mengyizhou95@gmail.com>
2018-02-04 14:33:04 -0500

    integrating with MySQL

--- src/main/java/edu/northeastern/cs5200/Cs5200Spring2018MengApplication.java
+++ src/main/java/edu/northeastern/cs5200/Cs5200Spring2018MengApplication.java
@@ -2,9 +2,18 @@ package edu.northeastern.cs5200;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200Spring2018MengApplication {
+public class Cs5200Spring2018MengApplication extends SpringBootServletInitializer {
+	
+	 @Override
+	 protected SpringApplicationBuilder
+	  configure(SpringApplicationBuilder application) {
+	  return application.sources(
+	   Cs5200Spring2018MengApplication.class);
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200Spring2018MengApplication.class, args);
